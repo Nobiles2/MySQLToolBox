f43b438e0ca30a33ba40d9416b8d9709f938936a
Adam Wnęk <warp24@op.pl>
2014-12-05 20:14:30 +0100

    Fix of getting serialized array values

--- getPhpSerializedArrayValueByKey.sql
+++ getPhpSerializedArrayValueByKey.sql
@@ -9,11 +9,11 @@ BEGIN
 		
 		@author Adam Wnęk (http://kredyty-chwilowki.pl/)
 		@licence MIT
-		@version 1.0
+		@version 1.1
 	*/
 	-- required variables
-	DECLARE __output_part,__output,__extra_byte_counter,__extra_byte_number,__value_type TEXT;
-	DECLARE __value_length,__char_ord,__start,__char_counter,__non_multibyte_length,__value_array_length,__value_array_open_counter INT SIGNED;
+	DECLARE __output_part,__output,__extra_byte_counter,__extra_byte_number,__value_type,__array_part_temp TEXT;
+	DECLARE __value_length,__char_ord,__start,__char_counter,__non_multibyte_length,__array_close_bracket_counter,__array_open_bracket_counter INT SIGNED;
 
 	SET __output := NULL;
 	
@@ -30,16 +30,28 @@ BEGIN
 		CASE 	
 		WHEN __value_type = 'a' THEN
 		
-			-- get array length gives information to which } we need to cut in first step
-			SET __value_array_length := SUBSTRING_INDEX(SUBSTRING_INDEX(__output_part, ':', 2),':',-1);
+			-- we get proper array by counting open and close brackets
+			SET __array_open_bracket_counter := 1;
+			SET __array_close_bracket_counter := 0;
+			-- without first open { so counter is 1
+			SET __array_part_temp := SUBSTRING(__output_part FROM LOCATE('{',__output_part)+1);
 			
-			SET __output := CONCAT(SUBSTRING_INDEX(__output_part,'}',__value_array_length),'}');
-			
-			-- get number of { because same number of } need to stay at the end
-			SET __value_array_open_counter := LENGTH(__output) - LENGTH(REPLACE(__output, '{', ''));
-					
-			-- from left to [__value_array_length] appearance of }
-			SET __output := CONCAT(SUBSTRING_INDEX(__output_part,'}',__value_array_open_counter),'}');
+			-- we start from first { and counting open and closet brackets until we find last closing one
+			WHILE (__array_open_bracket_counter > 0 OR LENGTH(__array_part_temp) = 0) DO
+				-- next { exists and its before closest }
+				IF LOCATE('{',__array_part_temp) > 0 AND (LOCATE('{',__array_part_temp) < LOCATE('}',__array_part_temp)) THEN
+					-- cut from found { + 1, to the end
+					SET __array_open_bracket_counter := __array_open_bracket_counter + 1;
+					SET __array_part_temp := SUBSTRING(__array_part_temp FROM LOCATE('{',__array_part_temp) + 1);					
+				ELSE
+					-- cut from found } + 1, to the end
+					SET __array_open_bracket_counter := __array_open_bracket_counter - 1;
+					SET __array_close_bracket_counter := __array_close_bracket_counter + 1;
+					SET __array_part_temp := SUBSTRING(__array_part_temp FROM LOCATE('}',__array_part_temp) + 1);					
+				END IF;
+			END WHILE;
+			-- final array is from beginning to [__array_close_bracket_counter] count of closing }
+			SET __output := CONCAT(SUBSTRING_INDEX(__output_part,'}',__array_close_bracket_counter),'}');
 		
 		WHEN __value_type = 'd' OR __value_type = 'i' OR __value_type = 'b' THEN
 			
